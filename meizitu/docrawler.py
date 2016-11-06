bdc8c9f9c5a5e93599b27d5ce1e7572aef798553
jeff <124303687@qq.com>
2016-11-06 22:54:59 +0800

    update meizitu

--- meizitu/docrawler.py
+++ meizitu/docrawler.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 
-import requests
 from bs4 import BeautifulSoup
+import requests
+import time
 import os
 
 
@@ -10,30 +11,34 @@ class DoCrawl(object):
     def __init__(self):
         self.start_url = "http://www.mzitu.com/all"
         self.do_request = DoRequest()
-        self.base_path = "/Users/Jeffrey/mm2"
+        self.base_path = "F:\mm2"
+        self.base_txt = self.base_path + "\crawlloggig.txt"
 
     def do_mkdir(self, title):
         title = title.strip()
-        isExists = os.path.exists(os.path.join(self.base_path, title))
-        if not isExists:
-            print('创建文件夹：', title)
+        is_exists = os.path.exists(os.path.join(self.base_path, title))
+        if not is_exists:
+            # print('创建文件夹：', title)
             os.mkdir(os.path.join(self.base_path, title))
-            return True
+            with open(self.base_txt, 'a') as w:
+                w.write(title + ' ' + time.ctime() + '\n')
+                return True
         else:
             # print('文件夹：', title, '已存在')
             return False
 
     def save_img(self, save_url, title):
+        title = title.replace('?', ' ')  # 防止title有问号，无法创建文件夹
         html = self.do_request.do_request(save_url)
         soup = BeautifulSoup(html.text, 'lxml')
         img_url = soup.find('div', class_='main-image').find('img')['src']
-        print('crawl :', img_url)
-        imgName = img_url[-9:]
+        print('crawl :', title, img_url)
+        img_name = img_url[-9:]
         img = self.do_request.do_request(img_url)
-        save_path = self.base_path + "/" + title + "/" + imgName
+        save_path = self.base_path + "\\" + title + "\\" + img_name
         with open(save_path, 'ab') as f:
             f.write(img.content)
-        print(img_url, '保存成功')
+        print('保存成功', time.ctime())
 
     def get_every_mm(self, get_url, title):
         page_html = self.do_request.do_request(get_url)
@@ -42,23 +47,16 @@ class DoCrawl(object):
         pagenum = page_cont.find('span').text
         for each in range(1, int(pagenum) + 1):
             crawl_url = get_url + '/' + str(each)
-            print('保存图片：', crawl_url)
             self.save_img(crawl_url, title)
-            break
 
     def do_main(self):
         start_html = self.do_request.do_request(self.start_url)
         all_a = BeautifulSoup(start_html.text, 'lxml').find("div", class_='all').find_all('a')
-        # do_url = ''
-        count = 1
         for a in all_a:
             title = a.get_text()
             do_url = a['href']
             self.do_mkdir(title)
             self.get_every_mm(do_url, title)
-            if count == 10:
-                break
-            count += 1
 
 
 class DoRequest(object):
