66253bc4c6c6256112713a6c42c87e58957defe0
jeff <124303687@qq.com>
2016-12-09 23:02:59 +0800

    写一些爬虫有用的工具

---
+++ spiderutils/timetools.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 2016/12/9 0009
+# JEFF
+
+import time
+import datetime
+import random
+
+
+# 获取一个格式化的当前时间
+def current_date():
+    return time.strftime("%Y-%m-%d %H:%M:%S")
