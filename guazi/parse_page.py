3fc469440f663917bcf2cf6a05dc45c71a5e8f5e
jeff <124303687@qq.com>
2017-01-18 22:50:01 +0800

    guazi

--- guazi/parse_page.py
+++ guazi/parse_page.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 # 2017/1/17
 
+import re
+from guazi.parse_request import requesturl
+
+
+class PageParse(object):
+    def parse_cat_url(self, url):
+        html = requesturl(url)
