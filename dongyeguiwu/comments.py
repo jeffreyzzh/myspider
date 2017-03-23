b2bb9befbf5525e009ed303cd287826d9a4fff0b
jeff <124303687@qq.com>
2017-03-23 23:08:51 +0800

    东野圭吾爬虫完成

--- dongyeguiwu/comments.py
+++ dongyeguiwu/comments.py
@@ -2,7 +2,11 @@
 # 17/3/21
 
 import requests
+import os
 import traceback
+
+desktop = os.path.join(os.path.expanduser("~"), 'Desktop')
+base_dir = os.path.join(desktop, 'dongyeguiwu')
 
 
 def downpage(url, trycount=1):
@@ -12,12 +16,26 @@ def downpage(url, trycount=1):
         resp = requests.get(url, timeout=3)
         return resp.text
     except Exception as e:
-        print(e)
-        print(traceback.format_exc())
+        # print(e)
+        # print(traceback.format_exc())
         return downpage(url, trycount=trycount + 1)
 
 
+def init_dir():
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+
+
+def book_path(bookname):
+    _book_path = os.path.join(base_dir, bookname)
+    if not os.path.exists(_book_path):
+        os.mkdir(_book_path)
+    return _book_path
+
+
 if __name__ == '__main__':
-    url = 'http://www.dongyeguiwu.com/books'
-    html = downpage(url)
-    print(html)
+    # url = 'http://www.dongyeguiwu.com/books'
+    # html = downpage(url)
+    # print(html)
+    p = book_path('白夜行')
+    print(p)
--- dongyeguiwu/domain.py
+++ dongyeguiwu/domain.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # 17/3/21
 
+
 from lxml import etree
 from dongyeguiwu.comments import *
 
@@ -16,45 +17,57 @@ def indexpage():
         exit()
     for each in books:
         bookinfo = each.xpath('./a')[0]
+        book_dir = book_path(bookinfo.text)
         print(bookinfo.get('href'))
         print(bookinfo.text)
+        handlebook(bookinfo.get('href'), book_dir)
 
 
-def handlebook(bookurl):
+def handlebook(bookurl, book_dir):
     book_page = downpage(bookurl)
     selector = etree.HTML(book_page)
     chapters = selector.xpath('//div[@class="booktable"]//li[@class="chapters"]/a')
     for each in chapters:
         print(each.get('href'))
         print(each.text)
+        handlechapter(each.get('href'), book_dir, each.text)
 
 
 def full_url(rurl, page):
     return rurl + '/' + str(page)
 
 
-def handlechapter(chapterurl):
+def handlechapter(chapterurl, book_dir, chaptername):
     chapter_page = downpage(chapterurl)
     selector = etree.HTML(chapter_page)
+    content = list()
     # 先判断有没分页
     have_fenye = selector.xpath('//div[@class="fenye"]')
     if have_fenye:
         # 有分页
         ps = selector.xpath('//div[@class="text"]//p')
         for each in ps:
-            print(each.text)
+            # print(each.text)
+            content.append(each.text)
         fenyes = have_fenye[0].xpath('./a')
         for page in range(2, len(fenyes)):
-            fenye_html = downpage(full_url(chapter_url, page))
+            fenye_html = downpage(full_url(chapterurl, page))
             xpaths = etree.HTML(fenye_html)
             ps = xpaths.xpath('//div[@class="text"]//p')
             for each in ps:
-                print(each.text)
+                # print(each.text)
+                content.append(each.text)
     else:
         # 无分页,直接处理
         ps = selector.xpath('//div[@class="text"]//p')
         for each in ps:
-            print(each.text)
+            # print(each.text)
+            content.append(each.text)
+    chapter_path = os.path.join(book_dir, '{}.txt'.format(chaptername))
+    with open(chapter_path, 'w', encoding='utf-8') as f:
+        for each in content:
+            f.write(each)
+            f.write('\r\n')
 
 
 if __name__ == '__main__':
@@ -64,6 +77,9 @@ if __name__ == '__main__':
     # url2 = 'http://www.dongyeguiwu.com/books/x'
     # handlebook(url2)
 
-    chapter_url = 'http://www.dongyeguiwu.com/books/baiyexing/65.html'
-    handlechapter(chapter_url)
+    # chapter_url = 'http://www.dongyeguiwu.com/books/baiyexing/53.html'
+    # handlechapter(chapter_url, None, None)
+
+    init_dir()
+    indexpage()
     pass
